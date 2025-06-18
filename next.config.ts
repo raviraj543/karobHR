9b45a5b46a4ece4af0525afff5fe67ffcc07cfa4
kr ravi <rr777286@gmail.com>
2025-06-18 07:28:45 +0000

    fix(pwa): Correct next-pwa config for custom service worker

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,12 @@ const withPWA = require('next-pwa')({
     dest: 'public',
     register: true,
     skipWaiting: true,
-    // Solution: Tell next-pwa exactly where to find the FCM service worker
-    runtimeCaching: [], // Disable default runtime caching if not needed
-    buildExcludes: [/middleware.ts$/],
-    swSrc: './public/firebase-messaging-sw.js', // Specify the path to your service worker file
-    customWorkerDir: 'public', // Directory where your custom service worker is
+    swSrc: 'public/firebase-messaging-sw.js',
+    customWorkerDir: 'public',
 });
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   compiler: {
     // ssr and displayName are configured by default
     styledComponents: true,
